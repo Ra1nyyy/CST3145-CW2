2d19bad8d07703d578eb920b8ec30a5f61bae552
Alexander Navarra <an942@live.mdx.ac.uk>
2022-03-03 00:36:29 +0000

    FS code added

--- server.js
+++ server.js
@@ -8,24 +8,24 @@ const res = require('express/lib/response')
 app.use(express.json())
 app.use (cors())
 
-// app.use(function (req, res, next) {
-//     console.log("Request URL: " + req.url);
-//     console.log("Request Date: " + new Date());
-//     next();
-// });
-// app.use(function (req, res, next) {
-//     // Uses path.join to find the path where the file should be
-//     var filePath = path.join(__dirname, 'static', req.url);
-//     // Built-in fs.stat gets info about a file
-//     fs.stat(filePath, function (err, fileInfo) {
-//         if (err) {
-//             next();
-//             return;
-//         }
-//         if (fileInfo.isFile()) res.sendFile(filePath);
-//         else next();
-//     });
-// });
+app.use(function (req, res, next) {
+    console.log("Request URL: " + req.url);
+    console.log("Request Date: " + new Date());
+    next();
+});
+app.use(function (req, res, next) {
+    // Uses path.join to find the path where the file should be
+    var filePath = path.join(__dirname, 'static', req.url);
+    // Built-in fs.stat gets info about a file
+    fs.stat(filePath, function (err, fileInfo) {
+        if (err) {
+            next();
+            return;
+        }
+        if (fileInfo.isFile()) res.sendFile(filePath);
+        else next();
+    });
+});
 
 
 const MongoClient = require('mongodb').MongoClient;
